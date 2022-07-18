dab3db9642f3a4b2bd474a971cbb61cdcb0b8de0
Kry5t14n <savcio9182@gmail.com>
2022-07-18 12:36:13 +0200

    update
    to do:
    - stars count

--- script.js
+++ script.js
@@ -1,14 +1,79 @@
 $(document).ready(function () {
+    class Movie {
+        constructor(title, genre, stars, id) {
+            this.title = title;
+            this.genre = genre;
+            this.stars = stars;
+            this.id = id;
+        }
+    }
+
+    const starsFunc = (id, title) => {
+        $(`.${id}${title}`).on('click', function () {
+            $(this).html("&#9733;");
+            let element = $(this);
+            while(element.prev().hasClass("star")) {
+                element = element.prev();
+                element.html("&#9733;");
+            }
+            element = $(this);
+            while(element.next().hasClass("star")) {
+                element = element.next();
+                element.html("&#9734;");
+            }
+        });
+    }
+
+    const closeBtn = (id, title) => {
+        $(`#${id}${title}`).on('click', function () {
+            $(this).parent().remove();
+            movies = movies.filter((mv) => mv.title != title);
+            localStorage.setItem('movies', JSON.stringify(movies));
+        });
+    }
+
+    const resetForm = () => {
+        $("#title").val('');
+        $("#genre").val('None');
+        $("#genre").removeClass("wrong");
+        $("#title").removeClass("wrong");
+    }
+
+    const displayMovie = (title, genre, id) => {
+        $(".section3").append(`
+            <article>
+                <span class="close-btn" id="${id}${title}">&#9932;</span>
+                <h2>${title}</h2>
+                <h3>${genre}</h3>
+                <div class="rating">
+                    <span class="star ${id}${title}">&#9734;</span>
+                    <span class="star ${id}${title}">&#9734;</span>
+                    <span class="star ${id}${title}">&#9734;</span>
+                    <span class="star ${id}${title}">&#9734;</span>
+                    <span class="star ${id}${title}">&#9734;</span>
+                </div>
+            </article>`);
+        
+        starsFunc(id, title);
+        closeBtn(id, title);
+    }
+
+    let movies = [];
+    movies = JSON.parse(localStorage.getItem('movies'));
+
+    console.log(movies);
+
+    movies.forEach(mv => {
+        displayMovie(mv.title, mv.genre, mv.Id)
+    });
+
     $("#add-btn").on('click', function () {
         $(".section2").removeClass("hidden");
     });
 
     $("#close-btn-form").on('click', function () {
         $(".section2").addClass("hidden");
-        $("#title").val('');
-        $("#genre").val('None');
-        $("#genre").removeClass("wrong");
-        $("#title").removeClass("wrong");
+        resetForm();
     });
 
     $("#add-form").on('click', function() {
@@ -16,44 +81,15 @@ $(document).ready(function () {
             let title = $("#title").val();
             let genre = $("#genre").val();
             let randomId = Math.round(Math.random()*100000);
-            $(".section3").append(`
-            <article>
-                <span class="close-btn" id="${randomId}${title}">&#9932;</span>
-                <h2>${title}</h2>
-                <h3>${genre}</h3>
-                <div class="rating">
-                    <span class="star ${randomId}${title}">&#9734;</span>
-                    <span class="star ${randomId}${title}">&#9734;</span>
-                    <span class="star ${randomId}${title}">&#9734;</span>
-                    <span class="star ${randomId}${title}">&#9734;</span>
-                    <span class="star ${randomId}${title}">&#9734;</span>
-                </div>
-            </article>`);
+            let stars = 0;
+            movies.push(new Movie(title, genre, stars, randomId));
+            localStorage.setItem('movies', JSON.stringify(movies));
 
-            $(`.${randomId}${title}`).on('click', function () {
-                $(this).html("&#9733;");
-                let element = $(this);
-                while(element.prev().hasClass("star")) {
-                    element = element.prev();
-                    element.html("&#9733;");
-                }
-                element = $(this);
-                while(element.next().hasClass("star")) {
-                    element = element.next();
-                    element.html("&#9734;");
-                }
-            });
-
-            $(`#${randomId}${title}`).on('click', function () {
-                $(this).parent().remove();
-            });
+            displayMovie(title, genre, randomId);
 
             $(".section2").addClass("hidden");
 
-            $("#title").val('');
-            $("#genre").val('None');
-            $("#genre").removeClass("wrong");
-            $("#title").removeClass("wrong");
+            resetForm();
         } else if($("#title").val() == '' & $("#genre").val() != 'None') {
             $("#genre").removeClass("wrong");
             $("#title").addClass("wrong");
